573a86d7012aa67bec5ef59b4d446faacdcd6b3c
J3 <giljr.2009@gmail.com>
2017-09-04 05:54:43 -0400

    code to send serial data from 2.4 G Radios - J3BCP
    Setp, 2017

---
+++ _35_Sending_Serial_Counter_12.ino
@@ -0,0 +1,32 @@
+/*
+   Project name: JayThree Balancing Car Project #arduSerie 35
+     Awesome project based on Your Arduino Balancing Robot (YABR) by Joop Brokking
+     Flavour VI - Working with gyroelerometer fo GY-85 Board
+     Hex File: _35_ArduinoNunchukDemo_09.ino
+   Revision History:
+     Sept, 2017
+       - Medium webpage: https://goo.gl/R25hLc  & https://goo.gl/dReMhG
+   Description:
+         Simple code to send counting data to the serial. 
+Simple code to send nunch data to the serial
+   MCU:                  Pro Mini Atmega328 16MHz 5V @16MHz -  https://www.sparkfun.com/products/11113
+   IDE:                  Arduino 1.8.3 - http://www.arduino.cc/
+   Nunchuk Controller:   https://goo.gl/6T3Dvs
+   Connections:          See Official youtube channel vids: https://goo.gl/R25hLc
+   Based on Lib:         Code by Gabriel Bianconi, https://github.com/GabrielBianconi/arduino-nunchuk
+   Credit:               Arduino Wii Nunchuck Tutorial https://youtu.be/vhJRR_7m6z4
+   WiiChuck Adapter:     https://www.sparkfun.com/products/9281
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License v3 as published by
+   the Free Software Foundation - There is No Warranty. Use at your own risk!
+*/
+int counter = 0;
+void setup() {
+    Serial.begin(9600);
+    delay(50);
+}
+void loop() {
+    counter++;
+    Serial.println(counter);
+    delay(500); // wait half a sec
+}
