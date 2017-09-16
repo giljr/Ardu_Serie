0ab3188a9fb6f1ae0323af8686e3228a70ea73dd
J3 <giljr.2009@gmail.com>
2017-09-16 10:53:34 -0400

    code to receive data from 2.4 G Radios - J3BCP
    Correcting title

--- _35_Receiving_Serial_Data_11.ino
+++ _35_Receiving_Serial_Data_11.ino
@@ -2,13 +2,13 @@
    Project name: JayThree Balancing Car Project #arduSerie 35
      Awesome project based on Your Arduino Balancing Robot (YABR) by Joop Brokking
      Flavour VI - Working with gyroelerometer fo GY-85 Board
-     Hex File: _35_ArduinoNunchukDemo_09.ino
+     Hex File: _35_Receiving_Serial_Data_11.ino
    Revision History:
      Sept, 2017
        - Medium webpage: https://goo.gl/R25hLc  & https://goo.gl/dReMhG
    Description:
-         Simple code to receive serial data. 
-Simple code to send nunch data to the serial
+         Simple code to receive serial data on Arduino Mini. 
+         Needs other Simple code to send nunch data to the serial from Arduino UNO.
    MCU:                  Pro Mini Atmega328 16MHz 5V @16MHz -  https://www.sparkfun.com/products/11113
    IDE:                  Arduino 1.8.3 - http://www.arduino.cc/
    Nunchuk Controller:   https://goo.gl/6T3Dvs
