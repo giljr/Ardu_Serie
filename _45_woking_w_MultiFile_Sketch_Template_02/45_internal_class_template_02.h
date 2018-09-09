4ca91ff64f34caa825bec99269809bdb4fc8ee63
J3 <giljr.2009@gmail.com>
2018-09-08 22:04:51 -0300

    Part_Template_A clean Coding Technique In Arduino IDE - ArduSerie#45
    Part_Template_A clean Coding Technique In Arduino IDE - ArduSerie#45

---
+++ _45_woking_w_MultiFile_Sketch_Template_02/45_internal_class_template_02.h
@@ -0,0 +1,26 @@
+class ___ {
+
+  private:
+
+    int __Pin;
+    int __Pin;
+
+
+  public:
+
+    void __Setup(int __Pin, int __Pin) {
+
+    }
+
+    void __Loop() {
+
+    }
+
+    void __Display() {
+
+    }
+
+};
+___ __;
+
+
