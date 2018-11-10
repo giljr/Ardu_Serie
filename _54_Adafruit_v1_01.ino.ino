16868fc93c25db0f104d077f175941f3b68768eb
J3 <giljr.2009@gmail.com>
2018-11-10 17:15:38 -0400

    #54 - Adafruit Motor Shield v1 & v2 - Part I
    #54 - Adafruit Motor Shield v1 & v2 - Part I

--- _54_Adafruit_v1_01.ino.ino
+++ _54_Adafruit_v1_01.ino.ino
@@ -1,19 +1,16 @@
 /*
-  Ardu-Serie#54
-  Project : Adafruit Motor Shield v1 & v2
+    Ardu-Serie#54
+  Project : Adafruit Motor Shield v1 & v2
             4 DC Motors or 2 Stepper Motor or 2 Servos -
-            1.2A@25v & 3.2A@15v peak
+            1.2A@25v & 3.2A@15v peak
   
   Ino File: _54_Adafruit_v1_01.ino
   
   Flavour 01
-
   Date: Nov 2018
-
   Description: Simply accelerates the DC Motor 5v forward to 10 ms 
                and turn the motor to the reverse; Then repeat.
-
-  Board: Adafruit Motor Shield v1
+  Board: Adafruit Motor Shield v1
   https://learn.adafruit.com/adafruit-motor-shield/overview
   
   Connections: See Fritzing in Jungletronics:
@@ -22,12 +19,13 @@
            with lots of help from the Arduino community.
            Visit https://learn.sparkfun.com/ for more tutorials
            Visit http://www.arduino.cc to learn more about Arduino.
-
   Terms of Use:  This program is free software: you can redistribute it and/or modify
                  it under the terms of the GNU General Public License v3 as published by
                  the Free Softw are Foundation - There is no guarantee whatsoever :)
                  Use at your own risk!
 */
+
+
 
 #include <AFMotor.h>                 //Include library AF Motor
 
